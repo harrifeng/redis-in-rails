e16e806fddfcf36b9707b734e2d6c5e115a4a081
Haoran Feng <haoran.feng@alo7.com>
2017-03-02 15:55:06 +0800

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+200_000.times do |i|
+  User.create!(
+    name: "#{Faker::Internet.user_name}#{i}",
+    email: Faker::Internet.user_name + i.to_s + "@#{Faker::Internet.domain_name}"
+  )
+end
